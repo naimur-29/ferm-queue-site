9a3fbc329c269ed1554c332e42c6a1696a3e2e90
Naim <skylike2904@gmail.com>
2022-09-08 21:23:29 +0600

    fixed env bug

--- server/app/config.py
+++ server/app/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_uri: str
+    database_uri: str = "mongodb://localhost:27017"
     
     class Config:
         env_file = '.env'
